b0f7c6594ff0398d0939c6884b4279f6ff85f418
Rogério Matos <38260207+RogerioMatos78@users.noreply.github.com>
2023-02-18 08:28:36 -0300

    Adicionei arquivos via Upload

---
+++ HelloGit/src/br/com/rogeriomatos/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.rogeriomatos.git;
+
+/**
+ * @author matos
+ * Exemplo de integração do Eclipe com o Git
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
---
+++ HelloGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author matos
+ *
+ */
+module HelloGit {
+}
